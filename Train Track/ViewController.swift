01f14e589c06b5460ad414ea6347986a8e745a79
amthna <mathena2@gamil.com>
2015-06-06 16:29:25 -0600

    Initial Commit

---
+++ Train Track/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Train Track
+//
+//  Created by Andrew Mathena on 6/6/15.
+//  Copyright (c) 2015 Andrew Mathena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
